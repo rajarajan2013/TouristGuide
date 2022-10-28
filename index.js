c799c85d145bdb55e20d83e9be0c19036fdf6006
rajarajan2013 <101817946+rajarajan2013@users.noreply.github.com>
2022-10-28 18:21:00 +0530

    final

---
+++ index.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const home = document.querySelector(".btn");
+home.addEventListener("click", function () {
+  location.href = "index.html";
+});
+
+const upselect = document.querySelector(".up");
+upselect.addEventListener("click", function () {
+  location.href = "up.html";
+  console.log(clicked);
+});
+const tnselect = document.querySelector(".tn");
+tnselect.addEventListener("click", function () {
+  location.href = "tn.html";
+});
+const wbselect = document.querySelector(".wb");
+wbselect.addEventListener("click", function () {
+  location.href = "wb.html";
+});
+const mselect = document.querySelector(".m");
+mselect.addEventListener("click", function () {
+  location.href = "m.html";
+});
+const rselect = document.querySelector(".r");
+rselect.addEventListener("click", function () {
+  location.href = "r.html";
+});
+const keselect = document.querySelector(".ke");
+keselect.addEventListener("click", function () {
+  location.href = "ke.html";
+});
+const kaselect = document.querySelector(".ka");
+kaselect.addEventListener("click", function () {
+  location.href = "ka.html";
+});
+const pselect = document.querySelector(".p");
+pselect.addEventListener("click", function () {
+  location.href = "p.html";
+});
+const hpselect = document.querySelector(".hp");
+hpselect.addEventListener("click", function () {
+  location.href = "hp.html";
+});
+const gujuselect = document.querySelector(".guju");
+gujuselect.addEventListener("click", function () {
+  location.href = "guju.html";
+});
